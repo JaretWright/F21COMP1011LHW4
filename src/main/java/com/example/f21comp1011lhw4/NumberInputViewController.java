e9bb194c3a3c9da9127a7339d1fac3356a61b530
JaretWright <jaret.wright@georgiancollege.ca>
2021-09-28 13:19:01 -0400

    Updated the controller to assess if the user entered a full, valid phone number based on the north american numbering plan

--- src/main/java/com/example/f21comp1011lhw4/NumberInputViewController.java
+++ src/main/java/com/example/f21comp1011lhw4/NumberInputViewController.java
@@ -1,4 +1,38 @@
 package com.example.f21comp1011lhw4;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class NumberInputViewController {
+
+    @FXML
+    private TextField phoneNumberTextField;
+
+    @FXML
+    private Label areaCodeLabel;
+
+    @FXML
+    private Label cityCodeLabel;
+
+    @FXML
+    private Label xxxxLabel;
+
+    @FXML
+    private Label errorMsgLabel;
+
+    @FXML
+    private void assessPhoneNumber()
+    {
+        String phoneNumber = phoneNumberTextField.getText();
+        if (phoneNumber.matches("[2-9][0-9][0-9][2-9]\\d{2}\\d{4}"))
+        {
+            errorMsgLabel.setText("valid phone number");
+        }
+        else
+        {
+            errorMsgLabel.setText("invalid phone number");
+        }
+    }
 }
+
